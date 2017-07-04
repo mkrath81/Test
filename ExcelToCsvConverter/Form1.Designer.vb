5272b2d527f230e710b20e4fa08a1bef06dcd6f0
Malaya Rath <RathMa@india.rapidigm.com>
2017-07-04 14:23:04 +0530

    this is first commit

---
+++ ExcelToCsvConverter/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmXlsToCsv
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.label4 = New System.Windows.Forms.Label()
+        Me.cmbSheet = New System.Windows.Forms.ComboBox()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.txtCsv = New System.Windows.Forms.TextBox()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.txtFolder = New System.Windows.Forms.TextBox()
+        Me.txtXlx = New System.Windows.Forms.TextBox()
+        Me.btnBrowsFolder = New System.Windows.Forms.Button()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnBrows = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.BackColor = System.Drawing.Color.AliceBlue
+        Me.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label5.Location = New System.Drawing.Point(19, 56)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(213, 26)
+        Me.label5.TabIndex = 23
+        Me.label5.Text = "Convert Excel to CSV"
+        '
+        'label4
+        '
+        Me.label4.AutoSize = True
+        Me.label4.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.label4.Location = New System.Drawing.Point(20, 138)
+        Me.label4.Name = "label4"
+        Me.label4.Size = New System.Drawing.Size(72, 13)
+        Me.label4.TabIndex = 22
+        Me.label4.Text = "Sheet Name :"
+        '
+        'cmbSheet
+        '
+        Me.cmbSheet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbSheet.FormattingEnabled = True
+        Me.cmbSheet.Location = New System.Drawing.Point(130, 135)
+        Me.cmbSheet.Name = "cmbSheet"
+        Me.cmbSheet.Size = New System.Drawing.Size(198, 21)
+        Me.cmbSheet.TabIndex = 21
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.label3.Location = New System.Drawing.Point(20, 229)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(90, 13)
+        Me.label3.TabIndex = 20
+        Me.label3.Text = "Output file name :"
+        '
+        'txtCsv
+        '
+        Me.txtCsv.Location = New System.Drawing.Point(130, 226)
+        Me.txtCsv.Name = "txtCsv"
+        Me.txtCsv.Size = New System.Drawing.Size(198, 20)
+        Me.txtCsv.TabIndex = 19
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.label2.Location = New System.Drawing.Point(20, 202)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(77, 13)
+        Me.label2.TabIndex = 18
+        Me.label2.Text = "Output Folder :"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.label1.Location = New System.Drawing.Point(20, 106)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(66, 13)
+        Me.label1.TabIndex = 17
+        Me.label1.Text = "Input Excel :"
+        '
+        'txtFolder
+        '
+        Me.txtFolder.Location = New System.Drawing.Point(130, 199)
+        Me.txtFolder.Name = "txtFolder"
+        Me.txtFolder.Size = New System.Drawing.Size(239, 20)
+        Me.txtFolder.TabIndex = 16
+        '
+        'txtXlx
+        '
+        Me.txtXlx.Location = New System.Drawing.Point(130, 106)
+        Me.txtXlx.Name = "txtXlx"
+        Me.txtXlx.Size = New System.Drawing.Size(239, 20)
+        Me.txtXlx.TabIndex = 15
+        '
+        'btnBrowsFolder
+        '
+        Me.btnBrowsFolder.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnBrowsFolder.Location = New System.Drawing.Point(375, 196)
+        Me.btnBrowsFolder.Name = "btnBrowsFolder"
+        Me.btnBrowsFolder.Size = New System.Drawing.Size(57, 23)
+        Me.btnBrowsFolder.TabIndex = 14
+        Me.btnBrowsFolder.Text = "Browse"
+        Me.btnBrowsFolder.UseVisualStyleBackColor = False
+        '
+        'btnConvert
+        '
+        Me.btnConvert.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnConvert.Location = New System.Drawing.Point(328, 252)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(104, 42)
+        Me.btnConvert.TabIndex = 13
+        Me.btnConvert.Text = "CONVERT"
+        Me.btnConvert.UseVisualStyleBackColor = False
+        '
+        'btnBrows
+        '
+        Me.btnBrows.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnBrows.Location = New System.Drawing.Point(375, 106)
+        Me.btnBrows.Name = "btnBrows"
+        Me.btnBrows.Size = New System.Drawing.Size(57, 23)
+        Me.btnBrows.TabIndex = 24
+        Me.btnBrows.Text = "Browse"
+        Me.btnBrows.UseVisualStyleBackColor = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'frmXlsToCsv
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(521, 350)
+        Me.Controls.Add(Me.btnBrows)
+        Me.Controls.Add(Me.label5)
+        Me.Controls.Add(Me.label4)
+        Me.Controls.Add(Me.cmbSheet)
+        Me.Controls.Add(Me.label3)
+        Me.Controls.Add(Me.txtCsv)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.txtFolder)
+        Me.Controls.Add(Me.txtXlx)
+        Me.Controls.Add(Me.btnBrowsFolder)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Name = "frmXlsToCsv"
+        Me.Text = "Excel To Csv"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents label5 As Label
+    Private WithEvents label4 As Label
+    Private WithEvents cmbSheet As ComboBox
+    Private WithEvents label3 As Label
+    Private WithEvents txtCsv As TextBox
+    Private WithEvents label2 As Label
+    Private WithEvents label1 As Label
+    Private WithEvents txtFolder As TextBox
+    Private WithEvents txtXlx As TextBox
+    Private WithEvents btnBrowsFolder As Button
+    Private WithEvents btnConvert As Button
+    Private WithEvents btnBrows As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ ExcelToCsvConverter/Form1.vb
@@ -0,0 +1,325 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports System.IO
+Imports Excel
+
+Public Class frmXlsToCsv
+    Dim result As DataSet = New DataSet
+    Private Sub btnBrows_Click(sender As Object, e As EventArgs) Handles btnBrows.Click
+        Dim Chosen_File As String = ""
+        If (OpenFileDialog1.ShowDialog = DialogResult.OK) Then
+            Chosen_File = OpenFileDialog1.FileName
+        End If
+
+        If (Chosen_File = String.Empty) Then
+            Return
+        End If
+
+        txtXlx.Text = Chosen_File
+        getExcelData(txtXlx.Text)
+    End Sub
+    Private Sub DefaultValues()
+        'txtXlx.Text = "C: \Users\rathma\Desktop\New folder\Sample Audit Trail.xlsx"
+        'cmbSheet.Text = "Sheet1"
+        'txtFolder.Text = "C: \Users\rathma\Desktop\New folder"
+        'txtCsv.Text = "test"
+    End Sub
+
+    Private Sub getExcelData(ByVal tfile As String)
+        If tfile.EndsWith(".xlsx") Then
+            ' Reading from a binary Excel file (format; *.xlsx)
+            Dim stream As FileStream = File.Open(tfile, FileMode.Open, FileAccess.Read)
+            Dim excelReader As IExcelDataReader = ExcelReaderFactory.CreateOpenXmlReader(stream)
+            result = excelReader.AsDataSet
+            excelReader.Close()
+        End If
+
+        If tfile.EndsWith(".xls") Then
+            ' Reading from a binary Excel file ('97-2003 format; *.xls)
+            Dim stream As FileStream = File.Open(tfile, FileMode.Open, FileAccess.Read)
+            Dim excelReader As IExcelDataReader = ExcelReaderFactory.CreateBinaryReader(stream)
+            result = excelReader.AsDataSet
+            excelReader.Close()
+        End If
+
+        Dim items As List(Of String) = New List(Of String)
+        Dim i As Integer = 0
+        Do While (i < result.Tables.Count)
+            items.Add(result.Tables(i).TableName.ToString)
+            i = (i + 1)
+        Loop
+
+        cmbSheet.DataSource = items
+    End Sub
+
+    Private Sub converToCSV(ByVal ind As Integer)
+        ' sheets in excel file becomes tables in dataset
+        'result.Tables[0].TableName.ToString(); // to get sheet name (table name)
+        Dim a As String = ""
+        Dim row_no As Integer = 0
+        'Dim dttbl As DataTable = result.Tables(0)
+        'result.Tables.Remove(dttbl)
+        'result.Tables.Add(GetTable)
+
+        Dim dttbl As DataTable = GetTable()
+
+
+        'for column1 :Employee Profile
+        Dim i As Integer = 0
+            Dim newTableRowCnt As Integer = 1
+        Do While (i < result.Tables(ind).Columns.Count)
+            If i = 0 Then
+
+                a = result.Tables(ind).Rows(row_no)(i).ToString.LastIndexOf(">")
+                Dim words As String() = result.Tables(ind).Rows(row_no)(i).ToString.Split(">")
+                If words.Length > 1 Then
+                    dttbl.Rows.Add()
+                    dttbl.Rows(newTableRowCnt)(0) = words(words.Length - 1).ToString
+                    newTableRowCnt = newTableRowCnt + 1
+                End If
+                row_no = (row_no + 1)
+
+            End If
+            If row_no = result.Tables(ind).Rows.Count Then
+                row_no = 0
+                i = i + 1
+                newTableRowCnt = 1
+                Exit Do
+            End If
+
+        Loop
+
+
+        'for column1 :Employee 
+        newTableRowCnt = 1
+        While newTableRowCnt < dttbl.Rows.Count - 1
+            i = 1
+            a = result.Tables(ind).Rows(row_no)(i).ToString
+                Dim words As String() = a.Split(":")
+                If words(0).ToLower = "employee" Then
+
+                dttbl.Rows(newTableRowCnt)(1) = words(words.Length - 1).ToString.Replace(",", " ")
+                a = result.Tables(ind).Rows(row_no + 1)(i).ToString
+                Dim words1 As String() = a.Split(":")
+                If words1(0).ToLower = "associate id" Then
+                    If (words1.Length = 3) Then
+                        dttbl.Rows(newTableRowCnt)(3) = words1(words1.Length - 1).ToString.Substring(0, 10)
+                        words1(1) = words1(1).ToString.Substring(0, 10)
+                        dttbl.Rows(newTableRowCnt)(2) = words1(1).ToString
+                    Else
+                        words1(1) = words1(1).ToString.Substring(0, 10)
+                        dttbl.Rows(newTableRowCnt)(2) = words1(1).ToString
+                    End If
+
+
+                End If
+                    newTableRowCnt = newTableRowCnt + 1
+                End If
+                row_no = (row_no + 1)
+
+
+                If row_no = result.Tables(ind).Rows.Count Then
+                    row_no = 0
+                    i = i + 1
+                    newTableRowCnt = 1
+                End If
+
+            End While
+
+        'for column1 :Action,Effective Date
+        newTableRowCnt = 1
+        row_no = 0
+
+        While newTableRowCnt < dttbl.Rows.Count - 1
+
+            i = 2
+
+            a = result.Tables(ind).Rows(row_no)(i).ToString
+            Dim words As String() = a.Split(":")
+            If words(0).ToLower = "action" Then
+
+                dttbl.Rows(newTableRowCnt)(4) = words(words.Length - 1).ToString
+                a = result.Tables(ind).Rows(row_no + 1)(i).ToString
+                Dim words1 As String() = a.Split(":")
+                If words1(0).ToLower = "effective date" Then
+
+                    dttbl.Rows(newTableRowCnt)(5) = words1(words1.Length - 1).ToString
+
+                End If
+
+                newTableRowCnt = newTableRowCnt + 1
+            End If
+            row_no = (row_no + 1)
+
+
+            If row_no = result.Tables(ind).Rows.Count Then
+                row_no = 0
+                i = i + 1
+                newTableRowCnt = 1
+            End If
+
+        End While
+
+        'for column1 :Change To
+        newTableRowCnt = 1
+        row_no = 0
+
+        While newTableRowCnt < dttbl.Rows.Count - 1
+
+            i = 3
+
+            a = result.Tables(ind).Rows(row_no)(i).ToString
+
+            If a <> "" And a <> "Changed To" Then
+
+                dttbl.Rows(newTableRowCnt)(6) = a.ToString
+                newTableRowCnt = newTableRowCnt + 1
+            End If
+            row_no = (row_no + 1)
+
+
+            If row_no = result.Tables(ind).Rows.Count Then
+                row_no = 0
+                i = i + 1
+                newTableRowCnt = 1
+                Exit While
+            End If
+
+        End While
+
+        'for column1 :User,Role,Date
+        newTableRowCnt = 1
+        row_no = 0
+
+        While newTableRowCnt < dttbl.Rows.Count - 1
+
+            i = 4
+
+            a = result.Tables(ind).Rows(row_no)(i).ToString
+
+            If a <> "" And a <> "Changed By" Then
+
+                dttbl.Rows(newTableRowCnt)(7) = a.ToString
+                Try
+                    a = result.Tables(ind).Rows(row_no + 1)(i).ToString
+                Catch ex As Exception
+                    Exit While
+                End Try
+
+                Dim words As String() = a.Split(":")
+                If words(0).ToLower = "role" Then
+
+                    dttbl.Rows(newTableRowCnt)(8) = words(words.Length - 1).ToString
+                    a = result.Tables(ind).Rows(row_no + 2)(i).ToString
+                    Dim words1 As String() = a.Split(":")
+                    If words1(0).ToLower = "date" Then
+                        words1(1) = words1(1).ToString.Substring(0, words1(1).ToString.Length - 3)
+                        dttbl.Rows(newTableRowCnt)(9) = words1(1).ToString
+
+                    End If
+
+                    newTableRowCnt = newTableRowCnt + 1
+                End If
+            End If
+            row_no = (row_no + 1)
+
+
+            If row_no = result.Tables(ind).Rows.Count Then
+                row_no = 0
+                i = i + 1
+                newTableRowCnt = 1
+                Exit While
+            End If
+
+        End While
+
+        'write data set to Excel
+
+        'to be removed
+        Dim dttbl1 As DataTable = result.Tables(0)
+        result.Tables.Remove(dttbl1)
+        result.Tables.Add(dttbl)
+        row_no = 0
+
+
+        a = ""
+        While (row_no < result.Tables(ind).Rows.Count)
+            i = 0
+            Do While (i < result.Tables(ind).Columns.Count)
+                a = (a _
+                            + (result.Tables(ind).Rows(row_no)(i).ToString + ","))
+                i = (i + 1)
+            Loop
+
+            row_no = (row_no + 1)
+            a = (a + "" & vbLf)
+
+        End While
+
+        Dim output As String = (txtFolder.Text + ("\" _
+                    + (txtCsv.Text + ".csv")))
+        Dim csv As StreamWriter = New StreamWriter(output, False)
+        csv.Write(a)
+        csv.Close()
+        MessageBox.Show("File converted succussfully")
+        txtXlx.Text = ""
+        txtFolder.Text = ""
+        txtCsv.Text = ""
+        cmbSheet.DataSource = Nothing
+        Return
+    End Sub
+
+    Private Sub btnBrowsFolder_Click(sender As Object, e As EventArgs) Handles btnBrowsFolder.Click
+        Dim result As DialogResult = Me.FolderBrowserDialog1.ShowDialog
+        Dim foldername As String = ""
+        If (result = DialogResult.OK) Then
+            foldername = Me.FolderBrowserDialog1.SelectedPath
+        End If
+
+        txtFolder.Text = foldername
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim fileName As String = ""
+        fileName = txtCsv.Text
+        If (fileName = "") Then
+            MessageBox.Show("Enter Valid file name")
+            Return
+        End If
+
+        converToCSV(cmbSheet.SelectedIndex)
+    End Sub
+
+    Function GetTable() As DataTable
+        ' Create new DataTable instance.
+        Dim table As New DataTable
+
+        ' Create four typed columns in the DataTable.
+        table.Columns.Add("Area Changed", GetType(String))
+        table.Columns.Add("Name", GetType(String))
+        table.Columns.Add("Associate ID", GetType(String))
+        table.Columns.Add("Position ID", GetType(String))
+        table.Columns.Add("Action", GetType(String))
+        table.Columns.Add("Effective Date", GetType(String))
+        table.Columns.Add("Changed To", GetType(String))
+        table.Columns.Add("Changed By", GetType(String))
+        table.Columns.Add("Change By Role", GetType(String))
+        table.Columns.Add("Processed Date", GetType(String))
+        ' Add five rows with those columns filled in the DataTable.
+        table.Rows.Add("Area Changed", "Name", "Associate ID", "Position ID", "Action", "Effective Date", "Changed To", "Changed By", "Change By Role", "Processed Date")
+
+        Return table
+    End Function
+
+    Private Sub frmXlsToCsv_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DefaultValues()
+    End Sub
+    Private Sub DatasetToXls()
+
+    End Sub
+End Class
